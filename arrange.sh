be756da441c8c8c84bcabe388edc4539e294185a
sanananan3 <127393132+sanananan3@users.noreply.github.com>
2023-04-20 21:53:11 +0900

    Update arrange.sh

---
+++ arrange.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+for file in *
+do
+  if [[ $file == [aA]* ]]
+  then
+    mv "$file" ../a/
+  elif [[ $file == [bB]* ]]
+  then
+    mv "$file" ../b/
+  elif [[ $file == [cC]* ]]
+  then
+    mv "$file" ../c/
+  elif [[ $file == [dD]* ]]
+  then
+    mv "$file" ../d/
+  elif [[ $file == [eE]* ]]
+  then
+    mv "$file" ../e/
+  elif [[ $file == [fF]* ]]
+  then
+    mv "$file" ../f/
+  elif [[ $file == [gG]* ]]
+  then
+    mv "$file" ../g/
+  elif [[ $file == [hH]* ]]
+  then
+    mv "$file" ../h/
+  elif [[ $file == [iI]* ]]
+  then
+    mv "$file" ../i/
+  elif [[ $file == [jJ]* ]]
+  then
+    mv "$file" ../j/
+  elif [[ $file == [kK]* ]]
+  then
+    mv "$file" ../k/
+  elif [[ $file == [kK]* ]]
+  then
+    mv "$file" ../k/
+  elif [[ $file == [lL]* ]]
+  then
+    mv "$file" ../l/
+  elif [[ $file == [mM]* ]]
+  then
+    mv "$file" ../m/
+  elif [[ $file == [nN]* ]]
+  then
+    mv "$file" ../n/
+  elif [[ $file == [oO]* ]]
+  then
+    mv "$file" ../o/
+  elif [[ $file == [pP]* ]]
+  then
+    mv "$file" ../p/
+  elif [[ $file == [qQ]* ]]
+  then
+    mv "$file" ../q/
+  elif [[ $file == [rR]* ]]
+  then
+    mv "$file" ../r/
+  elif [[ $file == [sS]* ]]
+  then
+    mv "$file" ../s/
+  elif [[ $file == [tT]* ]]
+  then
+    mv "$file" ../t/
+  elif [[ $file == [uU]* ]]
+  then
+    mv "$file" ../u/
+  elif [[ $file == [vV]* ]]
+  then
+    mv "$file" ../v/
+  elif [[ $file == [wW]* ]]
+  then
+    mv "$file" ../w/
+  elif [[ $file == [xX]* ]]
+  then
+    mv "$file" ../x/
+  elif [[ $file == [yY]* ]]
+  then
+    mv "$file" ../y/
+  elif [[ $file == [zZ]* ]]
+  then
+    mv "$file" ../z/
+
+  fi
+done
